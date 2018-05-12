8518acbade9ca9385666be4b97eb3b1289828d73
keniche <keniche1223@gmail.com>
2018-05-12 15:41:35 +0900

    Memoアプリに移行して作成

---
+++ app/src/main/java/android/lifeistech/com/memo/AddActivity.java
@@ -0,0 +1,39 @@
+//package android.lifeistech.com.memo;
+//
+//
+//import android.os.Bundle;
+//import android.support.v4.view.ViewPager;
+//import android.support.v7.app.AppCompatActivity;
+//
+//public class AddActivity extends AppCompatActivity {
+//    static final int REQUEST_CODE_GALLERY = 1;
+//    static final int REQUEST_CODE_CAMERA = 2;
+//
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_add);
+//
+//        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+//
+//
+//
+//        int[] imageList ={R.drawable.icon_grey_1,
+//                R.drawable.icon_grey_2,
+//                R.drawable.icon_grey_3,
+//                R.drawable.icon_grey_4,
+//                R.drawable.icon_grey_5};
+//
+//        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), imageList);
+//        viewPager.setAdapter(pagerAdapter);
+//
+//    }
+//
+//    //ギャラリーとカメラから情報を取ってくるメソッド
+//
+//
+//
+//
+//}
---
+++ app/src/main/java/android/lifeistech/com/memo/Memo.java
@@ -0,0 +1,15 @@
+package android.lifeistech.com.memo;
+
+import io.realm.RealmObject;
+
+public class Memo extends RealmObject{
+    // タイトル
+    // 日付
+    // 内容
+
+    public String title;
+    public String updateDate;
+    public String content;
+}
+
+
