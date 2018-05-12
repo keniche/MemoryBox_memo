a1eb016320ecedd0fa24954899f6e499aa206881
keniche <keniche1223@gmail.com>
2018-05-12 16:17:35 +0900

    コードの移行完了。Realmも正常に稼働

---
+++ app/src/main/java/android/lifeistech/com/memo/FragmentAdapter.java
@@ -0,0 +1,31 @@
+package android.lifeistech.com.memo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private int[] mImageList;
+
+    public FragmentAdapter(FragmentManager fm, int[] imageList) {
+        super(fm);
+        mImageList = imageList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        MemoryFragment fragment = new MemoryFragment();
+        Bundle args = new Bundle();
+        args.putInt("image_id", mImageList[position]);
+        args.putInt("position", position);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageList.length;
+    }
+}
